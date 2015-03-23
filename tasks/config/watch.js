d0b4cbc7c4d6608654f23d0dfab1d6540484132e
Matteo Abis <gmatteo..abis@gmail.com>
2015-03-23 10:34:52 +0100

    remove frontend grunt tasks

--- tasks/config/watch.js
+++ tasks/config/watch.js
@@ -20,14 +20,6 @@ module.exports = function(grunt) {
 			// API files to watch:
 			files: ['api/**/*']
 		},
-		assets: {
-
-			// Assets to watch:
-			files: ['assets/**/*', 'tasks/pipeline.js'],
-
-			// When assets are changed:
-			tasks: ['syncAssets' , 'linkAssets']
-		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-watch');
